af18aec771600b82e2947118ee92a2b80ce6116f
Diamondbanana420 <benjamesbradshaw22@gmail.com>
2026-02-09 03:11:11 +1000

    Add backend/dropship/automation/__init__.py

---
+++ backend/dropship/automation/__init__.py
@@ -0,0 +1,6 @@
+from .shopify_client import ShopifyClient
+from .pricing import PricingEngine
+from .inventory import InventoryManager
+from .fulfillment import FulfillmentEngine
+
+__all__ = ['ShopifyClient', 'PricingEngine', 'InventoryManager', 'FulfillmentEngine']
